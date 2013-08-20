0d4017aaddd539674e4710c2586233a1dc26b70b
mapan <mapan0817@gmail.com>
2013-08-20 10:18:44 -0700

    Create sum_root_leaf.java

---
+++ sum_root_leaf.java
@@ -0,0 +1,21 @@
+// path is the sum from root to the current node  
+ // sum is the sum of all root-to-leaf numbers up to the current node  
+ private int sumNumbersHelper(TreeNode root, int path, int sum) {  
+   if (root.left == null && root.right == null) { // reach a leaf  
+     return sum+path;  
+   }  
+   
+   if (root.left != null) { // go to left subtree  
+     sum = sumNumbersHelper(root.left, path*10+root.left.val, sum);  
+   }  
+   if (root.right != null) { // go to right subtree  
+     sum = sumNumbersHelper(root.right, path*10+root.right.val, sum);  
+   }  
+   
+   return sum;  
+ }  
+   
+ public int sumNumbers(TreeNode root) {  
+   if (root == null) return 0;  
+   return sumNumbersHelper(root, root.val, 0);  
+ }
