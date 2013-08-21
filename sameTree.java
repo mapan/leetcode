7b5954629dc8ff16aa353811ff0ce84d32817833
mapan <mapan0817@gmail.com>
2013-08-21 19:01:49 -0400

    Update sameTree.java

--- sameTree.java
+++ sameTree.java
@@ -2,12 +2,10 @@ public class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        if(p==null&&q==null) return true;
-        if(p==null||q==null) return false;
-        if(p.val!=q.val) return false;
-        boolean left=isSameTree(p.left,q.left);
-        if(!left) return left;
-        boolean right=isSameTree(p.right,q.right);
-        return right;
+        if(p==null) return q==null;
+        if(q==null) return false;
+        //if(p==null&&q==null) return true;
+        //if(p==null||q==null) return false;
+        return (p.val==q.val)&&(isSameTree(p.left,q.left))&&(isSameTree(p.right,q.right));
     }
 }
