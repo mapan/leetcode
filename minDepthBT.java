69f12e455cee7e5c9aef8c3ad6872637513d1624
mapan <mapan0817@gmail.com>
2013-08-24 00:21:39 -0400

    Update minDepthBT.java

--- minDepthBT.java
+++ minDepthBT.java
@@ -21,6 +21,24 @@ public class Solution {
     public int minDepth(TreeNode root) {
         // Start typing your Java solution below
         // DO NOT write main() function
+        if(root==null) return 0;
+        return m(root);
+    }
+    public int m(TreeNode root) {
+        if(root==null) {
+            return Integer.MAX_VALUE;
+        }
+        if(root.left==null&&root.right==null) return 1;
+        int left=m(root.left);
+        int right=m(root.right);
+        return Math.min(left,right)+1;
+    }
+}
+
+public class Solution {
+    public int minDepth(TreeNode root) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
         return min(root);
     }
     public int min(TreeNode root) {
