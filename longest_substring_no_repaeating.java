d85c7a184cd25b04b36242e117bde778ff32bd42
mapan <mapan0817@gmail.com>
2013-08-29 14:38:59 -0400

    Create longest_substring_no_repaeating.java

---
+++ longest_substring_no_repaeating.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if(s.length()<=1) return s.length();
+        HashMap<Character,Integer> h=new HashMap<Character,Integer>();
+        int curmax=0,max=0;
+        for(int i=0;i<s.length();i++) {
+            char c=s.charAt(i);
+            if(h.containsKey(c)) {     
+                if(i-h.get(c)<curmax) {  // previous is in the middle of curmax, then restart from the previous next char
+                    curmax=1;           // otherwise previous is the start of curmax, then simply replace current with previous
+                    i=h.get(c)+1;     
+                    c=s.charAt(i);
+                    h.clear();
+                }
+            }
+            else {
+                curmax++;
+            }
+            h.put(c,i);
+            max=curmax>max?curmax:max;
+        }
+        return max;
+    }
+}
+If we hit a recurrence of a character, it means that we find a substring without repeating characters (ended before this character). 
+Suppose we start from s[i] and find out the s[j] is same as s[k], where k<j. That said, s[i..j-1] is a substring without repeats. 
+That also tells us that there is no repeating characters between k+1 and j.
+http://n00tc0d3r.blogspot.com/2013/04/longest-substring-without-repeating.html
