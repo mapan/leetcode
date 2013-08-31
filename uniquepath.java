dc11969c1982626e140f2f0e7fab812ad47e812f
mapan <mapan0817@gmail.com>
2013-08-31 18:35:06 -0400

    Update uniquepath.java

--- uniquepath.java
+++ uniquepath.java
@@ -65,3 +65,31 @@ public class Solution {
         return num[n];
     }
 }
+
+
+int m=obstacleGrid.length;
+        int n=obstacleGrid[0].length;
+        /*int min=Math.min(m,n);
+        int[] num=new int[min+1];
+        for(int i=1;i<=m;i++) {
+            for(int j=1;j<=n;j++) {
+                if(i==1||j==1) 
+                    if(obstacleGrid
+                    num[j]=1;
+                else
+                    num[j]+=num[j-1];
+            }
+        }
+        return num[n];*/
+        int[][] num=new int[m+1][n+1];
+        for(int i=1;i<=m;i++) {
+            for(int j=1;j<=n;j++) {
+                if(i==1||j==1)
+                    if(obstacleGrid[i][j]==0)
+                        num[i][j]=1;
+                else
+                    if(obstacleGrid[i][j]!=1)
+                        num[i][j]=num[i-1][j]+num[i][j-1];
+            }
+        }
+        return num[m][n];
