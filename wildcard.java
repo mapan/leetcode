2ea6c0c5bf0caf4532bfc2a568667e8a85b52feb
mapan <mapan0817@gmail.com>
2013-09-04 19:03:41 -0400

    Create wildcard.java

---
+++ wildcard.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public boolean isMatch(String s, String p) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if(s.equals("")) return true;
+        
+        if(p.charAt(0)=='*') {
+            while(s
+        }
+    }
+}
