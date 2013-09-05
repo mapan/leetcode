06a547fdb44102839ad8fba322cf9c06426581b5
mapan <mapan0817@gmail.com>
2013-09-05 19:50:20 -0400

    Update parentheses.java

--- parentheses.java
+++ parentheses.java
@@ -28,3 +28,23 @@ public class Solution {
 }
 
 http://n00tc0d3r.blogspot.com/2013/04/longest-valid-parentheses.html
+
+public class Solution {
+    ArrayList<String> r;
+    public ArrayList<String> generateParenthesis(int n) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        r=new ArrayList<String>();
+        g("",n,n);
+        return r;
+    }
+    public void g(String sofar,int left,int right) {
+        if(left==0&&right==0) {
+            String cp=new String(sofar);
+            r.add(cp);
+            return;
+        }
+        if(left>0) g(sofar+"(",left-1,right);
+        if(left<right) g(sofar+")",left,right-1);
+    }
+}
