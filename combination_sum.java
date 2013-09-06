af0592062c50ac2c0cbc60188c6baff1aca926d6
mapan <mapan0817@gmail.com>
2013-09-05 22:47:38 -0400

    Update combination_sum.java

--- combination_sum.java
+++ combination_sum.java
@@ -24,3 +24,38 @@ public class Solution {
         }
     }
 }
+
+
+public class Solution {
+    ArrayList<ArrayList<Integer>> r;
+    ArrayList<Integer> d;
+    public ArrayList<ArrayList<Integer>> combinationSum2(int[] candidates, int target) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        r=new ArrayList<ArrayList<Integer>>();
+        d=new ArrayList<Integer>();
+        ArrayList<Integer> sofar=new ArrayList<Integer>();
+        Arrays.sort(candidates);
+        c(candidates,target,sofar,0);
+        return r;
+    }
+    public void c(int[] cand,int target,ArrayList<Integer> sofar,int pre) {
+        if(target==0) {
+            ArrayList<Integer> cp=new ArrayList<Integer>(sofar);
+            r.add(cp);
+            return;
+        }
+        if(target<cand[0]) return;
+        for(int i=0;i<cand.length;i++) {
+            if(cand[i]<pre||d.contains(cand[i])) continue;
+            if (i>start && candidates[i] == candidates[i-1]) continue;
+            sofar.add(cand[i]);
+            d.add(cand[i]);
+            c(cand,target-cand[i],sofar,cand[i]);
+            sofar.remove(sofar.size()-1);
+            //d.remove(cand[i]);
+            //d.remove((Integer)cand[i]);
+            d.remove(d.size()-1);
+        }
+    }
+}
