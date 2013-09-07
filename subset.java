5639112bb9e02b47efbcaf80bc019ec28f785885
mapan <mapan0817@gmail.com>
2013-09-07 14:45:57 -0400

    Update subset.java

--- subset.java
+++ subset.java
@@ -15,8 +15,39 @@ public class Solution {
             r.add(cp);
             return;
         }
-        if(ind>0&&S[ind]==S[ind-1]) 
+        //if(ind>0&&S[ind]==S[ind-1]) 
+            //s(S,ind+1,sofar);
+        //else {
+            sofar.add(S[ind]);
             s(S,ind+1,sofar);
+            sofar.remove(sofar.size()-1);
+            s(S,ind+1,sofar);
+        //}
+    }
+}
+
+public class Solution {
+    ArrayList<ArrayList<Integer>> r;
+    public ArrayList<ArrayList<Integer>> subsetsWithDup(int[] num) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        r=new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> sofar=new ArrayList<Integer>();
+        Arrays.sort(num);
+        s(num,0,sofar);
+        return r;
+    }
+    public void s(int[] S,int ind,ArrayList<Integer> sofar) {
+        if(ind==S.length) {
+            ArrayList<Integer> cp=new ArrayList<Integer>(sofar);
+            r.add(cp);
+            return;
+        }
+        if(sofar.contains(S[ind])) {
+            sofar.add(S[ind]);
+            s(S,ind+1,sofar);
+            sofar.remove(sofar.size()-1);
+        }
         else {
             sofar.add(S[ind]);
             s(S,ind+1,sofar);
