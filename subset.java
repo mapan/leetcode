68bdaab4aaf0d19fd673f03616a0cf57058052a0
mapan <mapan0817@gmail.com>
2013-09-07 14:19:26 -0400

    Create subset.java

---
+++ subset.java
@@ -0,0 +1,27 @@
+public class Solution {
+    ArrayList<ArrayList<Integer>> r;
+    public ArrayList<ArrayList<Integer>> subsets(int[] S) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        r=new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> sofar=new ArrayList<Integer>();
+        Arrays.sort(S);
+        s(S,0,sofar);
+        return r;
+    }
+    public void s(int[] S,int ind,ArrayList<Integer> sofar) {
+        if(ind==S.length) {
+            ArrayList<Integer> cp=new ArrayList<Integer>(sofar);
+            r.add(cp);
+            return;
+        }
+        if(ind>0&&S[ind]==S[ind-1]) 
+            s(S,ind+1,sofar);
+        else {
+            sofar.add(S[ind]);
+            s(S,ind+1,sofar);
+            sofar.remove(sofar.size()-1);
+            s(S,ind+1,sofar);
+        }
+    }
+}
