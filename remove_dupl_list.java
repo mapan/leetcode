14f51e38768e91c381bbda9009c69a908e5bc1ec
mapan <mapan0817@gmail.com>
2013-09-07 12:22:17 -0400

    Create remove_dupl_list.java

---
+++ remove_dupl_list.java
@@ -0,0 +1,69 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if(head==null||head.next==null) return head;
+        ListNode pre=head,cur=head.next;
+        while(cur!=null) {
+            if(cur.val==pre.val) {
+                pre.next=cur.next;
+                cur.next=null;
+                cur=pre.next;
+            }
+            else {
+                pre=cur;
+                cur=cur.next;
+            }
+        }
+        return head;
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if(head==null||head.next==null) return head;
+        ListNode dummy=new ListNode(0);
+        dummy.next=head;
+        ListNode pre=dummy,cur=head,next=head.next;
+        while(cur!=null) {
+            if(next!=null&&next.val==cur.val) {
+                while(next!=null&&next.val==cur.val) {
+                    next=next.next;
+                }
+                pre.next=next;
+                cur=next;
+                if(next!=null) next=next.next;
+            }
+            else {
+                pre=cur;
+                cur=next;
+                if(next!=null) next=next.next;
+            }
+        }
+        return dummy.next;
+    }
+}
