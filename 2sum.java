d64766e4f84b2e43f08a174dc8d964d5178dc7d1
mapan <mapan0817@gmail.com>
2013-09-07 17:53:55 -0400

    Update 2sum.java

--- 2sum.java
+++ 2sum.java
@@ -3,13 +3,16 @@ public class Solution {
         // Start typing your Java solution below
         // DO NOT write main() function
         HashMap<Integer,Integer> h=new HashMap<Integer,Integer>();
-        for(int i=0;i<numbers.length;i++)
-            h.put(numbers[i],i);
+        int[] r=new int[2];
         for(int i=0;i<numbers.length;i++) {
             int j=target-numbers[i];
-            if(h.containsKey(j))
-                return new int[]{Math.min(i+1,h.get(j)+1),Math.max(i+1,h.get(j)+1)};
+            if(h.containsKey(j)) {
+                r[0]=h.get(j)+1;
+                r[1]=i+1;
+                break;
+            }
+            else h.put(numbers[i],i);
         }
-        return null;
+        return r;
     }
 }
