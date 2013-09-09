cfe0848ff78e8baf2b5b255232fc9940c00b58c9
mapan <mapan0817@gmail.com>
2013-09-09 18:31:05 -0400

    Update range_search.java

--- range_search.java
+++ range_search.java
@@ -25,3 +25,40 @@ public class Solution {
         return l;
     }
 }
+
+class Solution {
+public:
+    vector<int> searchRange(int A[], int n, int target) {
+        vector<int> range(2, -1);
+        int lower = 0;
+        int upper = n;
+        int mid;
+
+        // Search for lower bound
+        while (lower < upper) {
+            mid = (lower + upper) / 2;
+            if (A[mid] < target)
+                lower = mid + 1;
+            else
+                upper = mid;
+        }
+
+        // If the target is not found, return (-1, -1)
+        if (A[lower] != target)
+            return range;
+        range[0] = lower;
+
+        // Search for upper bound
+        upper = n;
+        while (lower < upper) {
+            mid = (lower + upper) / 2;
+            if (A[mid] > target)
+                upper = mid;
+            else
+                lower = mid + 1;
+        }
+        range[1] = upper - 1;
+
+        return range;
+    }
+};
