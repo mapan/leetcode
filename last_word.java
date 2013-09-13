4b5f28c03a0d5d0c83a0b88665baf9ec2dec3208
mapan <mapan0817@gmail.com>
2013-09-13 15:58:52 -0400

    Update last_word.java

--- last_word.java
+++ last_word.java
@@ -2,6 +2,18 @@ public class Solution {
     public int lengthOfLastWord(String s) {
         // Start typing your Java solution below
         // DO NOT write main() function
+        int i=s.length()-1,c=0;
+        while(i>=0&&s.charAt(i)==' ') i--;
+        while(i>=0&&s.charAt(i)!=' ') {c++;i--;}
+        return c;
+    }
+}
+
+
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
         int c=0,len=s.length(),i=len-1;
         if(len==0) return c;
         while(i>=0) {
