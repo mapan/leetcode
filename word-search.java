c8a3e2fb9b6c0cc03baa4b600feb0b23f6e3c0ee
mapan <mapan0817@gmail.com>
2013-09-13 12:00:00 -0400

    Create word-search.java

---
+++ word-search.java
@@ -0,0 +1,39 @@
+public class Solution {
+    boolean[][] bo;
+    public boolean exist(char[][] board, String word) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        int r=board.length;
+        if(word.length()==0) return false;
+        if(r==0) {
+            if(word.length()==0) return true;
+            return false;
+        }
+        int c=board[0].length;
+        bo=new boolean[r][c];
+        for(int i=0;i<r;i++) {
+            for(int j=0;j<c;j++) {
+                if(board[i][j]==word.charAt(0)) {
+                    if(find(board,word,i,j)) return true;
+                }
+            }
+        }
+        return false;
+    }
+    public boolean find(char[][] b,String word,int row,int col) {
+        if(word.length()==0) return true;
+        if(row<0||row>=b.length||col<0||col>=b[0].length) return false;
+        if(bo[row][col]) return false;
+        if(word.charAt(0)!=b[row][col]) return false;
+        bo[row][col]=true;
+        boolean f=find(b,word.substring(1),row+1,col);
+        if(!f)//&&!bo[row-1][col])
+            f=find(b,word.substring(1),row-1,col);
+        if(!f)//&&!bo[row][col+1])
+            f=find(b,word.substring(1),row,col+1);
+        if(!f)//&&!bo[row][col-1])
+            f=find(b,word.substring(1),row,col-1);
+        bo[row][col]=false;
+        return f;
+    }
+}
