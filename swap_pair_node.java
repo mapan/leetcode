e1a8d5a025033fe7a6efe9f71481f303f7f80f3c
mapan <mapan0817@gmail.com>
2013-09-14 17:18:24 -0400

    Update swap_pair_node.java

--- swap_pair_node.java
+++ swap_pair_node.java
@@ -4,6 +4,26 @@ public class Solution {
         // Start typing your Java solution below
         // DO NOT write main() function
         if(head==null||head.next==null) return head;
+        ListNode d=new ListNode(1);
+        d.next=head;
+        ListNode pre=d,cur=head,next=head.next;
+        while(cur!=null&&cur.next!=null) {
+            cur.next=next.next;
+            next.next=cur;
+            pre.next=next;
+            pre=cur;
+            cur=cur.next;
+            if(cur!=null) next=cur.next;
+        }
+        return d.next;
+    }
+}
+
+public class Solution {
+    public ListNode swapPairs(ListNode head) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if(head==null||head.next==null) return head;
         ListNode p=null,pre=head,cur=head.next,next=cur.next;
         int ind=1;
         while(cur!=null) {
@@ -25,22 +45,3 @@ public class Solution {
     }
 }
 
-public class Solution {
-    public ListNode swapPairs(ListNode head) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        if(head==null||head.next==null) return head;
-        ListNode d=new ListNode(1);
-        d.next=head;
-        ListNode pre=d,cur=head,next=head.next;
-        while(cur!=null&&cur.next!=null) {
-            cur.next=next.next;
-            next.next=cur;
-            pre.next=next;
-            pre=cur;
-            cur=cur.next;
-            if(cur!=null) next=cur.next;
-        }
-        return d.next;
-    }
-}
