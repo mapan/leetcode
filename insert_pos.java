c4f9c787bd2e0fdc29b16f2797e4b18be851f1b3
mapan <mapan0817@gmail.com>
2013-09-19 15:46:16 -0400

    Update insert_pos.java

--- insert_pos.java
+++ insert_pos.java
@@ -3,6 +3,23 @@ public class Solution {
         // Start typing your Java solution below
         // DO NOT write main() function
         int l=0,r=A.length-1;
+        while(l<=r) {
+            int mid=l+(r-l)/2;
+            if(A[mid]>target) r=mid-1;
+            else if(A[mid]<target) l=mid+1;
+            else return mid;
+        }
+        //if(target>A[l]) return l+1;
+        return l;
+    }
+}
+
+
+public class Solution {
+    public int searchInsert(int[] A, int target) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        int l=0,r=A.length-1;
         while(l<r) {
             int mid=l+(r-l)/2;
             if(A[mid]>target) r=mid-1;
