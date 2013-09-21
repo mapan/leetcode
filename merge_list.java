7c0e5709b08303993c0f83e75c5a032f646695fc
mapan <mapan0817@gmail.com>
2013-09-21 16:38:41 -0400

    Update merge_list.java

--- merge_list.java
+++ merge_list.java
@@ -1,3 +1,25 @@
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        ListNode d=new ListNode(0);
+        ListNode p=d;
+        while(l1!=null&&l2!=null) {
+            if(l1.val<l2.val) {
+                p.next=l1;
+                l1=l1.next;
+            }
+            else {
+                p.next=l2;
+                l2=l2.next;
+            }
+            p=p.next;
+        }
+        p.next=(l1!=null?l1:l2);
+        return d.next;
+    }
+}
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
