a661995fd39072c75e1463f5a96c87edc9975619
mapan <mapan0817@gmail.com>
2013-09-21 13:27:30 -0400

    Update max_subarray.java

--- max_subarray.java
+++ max_subarray.java
@@ -11,3 +11,19 @@ public class Solution {
         return max;
     }
 }
+
+int maxSubArray(int A[], int n) {
+    return maxSub(A,0,n-1);
+}
+int maxSub(int A[], int left, int right){
+    if(left>right)return INT_MIN;
+    if(left==right)return A[left];
+    int mid=left+((right-left)>>1);
+    int leftMax=INT_MIN, rightMax=INT_MIN;
+    for(int i=mid-1, currSum=0; i>=left; --i)
+        currSum+=A[i], leftMax=max(leftMax, currSum);
+    for(int i=mid+1, currSum=0; i<=right; ++i)
+        currSum+=A[i], rightMax=max(rightMax, currSum);
+    int midMax=A[mid]+max(leftMax,0)+max(rightMax,0);
+    return max(max(maxSub(A,left,mid-1),maxSub(A,mid+1,right)),midMax);
+}
