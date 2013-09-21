daad26f7a1c2e9cc3b80bf39e5412fdfede425ee
mapan <mapan0817@gmail.com>
2013-09-21 16:19:23 -0400

    Update rotate_list.java

--- rotate_list.java
+++ rotate_list.java
@@ -1,3 +1,30 @@
+
+public class Solution {
+    public ListNode rotateRight(ListNode head, int n) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        ListNode d=new ListNode(0);
+        d.next=head;
+        int i=0;
+        ListNode n1=head,n2=head;
+        if(n1==null||n1.next==null) return head;
+        //n%=len;
+        while(i++<n) {
+            n2=n2.next;
+            if(n2==null) n2=head;
+        }
+        while(n2.next!=null) {
+            n1=n1.next;
+            n2=n2.next;
+        }
+        n2.next=d.next;
+        d.next=n1.next;
+        n1.next=null;
+        return d.next;
+    }
+}
+
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
