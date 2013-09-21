15bb2e12a7ca19db77e0002060fdb63b71da282b
mapan <mapan0817@gmail.com>
2013-09-21 14:35:23 -0400

    Create merge-interval.java

---
+++ merge-interval.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+public class Solution {
+    public ArrayList<Interval> merge(ArrayList<Interval> intervals) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        Comparator<Interval> comparator = new Comparator<Interval>() {  
+         @Override  
+         public int compare(Interval i1, Interval i2) {  
+           if (i1.start < i2.start) {  
+             return -1;  
+           } else {  
+             return 1;  
+           }  
+         }  
+       };  
+       Collections.sort(intervals, comparator); 
+       return m(intervals);
+    }
+    public ArrayList<Interval> m(ArrayList<Interval> intervals) {  
+       ArrayList<Interval> results = new ArrayList<Interval>();  
+       Interval cur = null,next;  
+       for (int i=0; i<intervals.size();) {  
+         cur=intervals.get(i);
+         int j=i+1;
+         while(j<intervals.size()&&cur.end>=intervals.get(j).start) {
+             cur.end=Math.max(cur.end,intervals.get(j).end);
+             j++;
+         }
+         results.add(cur);
+         i=j;
+       }  
+       return results;  
+    }
+}
