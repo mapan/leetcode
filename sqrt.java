b62d47e71d1181edfb5a603bacbc966e4340ad5a
mapan <mapan0817@gmail.com>
2013-09-22 15:32:17 -0400

    Create sqrt.java

---
+++ sqrt.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int sqrt(int x) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if (x <= 2) return x;
+        int low = 1, high = x;
+        while (low<=high) {
+        int mid = low + (high - low) / 2;
+        int div = x / mid;
+        if (mid == div) return mid;
+        if (mid < div) low = mid+1;
+        else high = mid-1;
+        }
+        
+        return high;
+    }
+}
