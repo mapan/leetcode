1b63e9bd1253c80a7f2e00587e715ab1c72a6cf3
mapan <mapan0817@gmail.com>
2013-09-22 18:00:34 -0400

    Update search-2d-matrix.java

--- search-2d-matrix.java
+++ search-2d-matrix.java
@@ -2,6 +2,31 @@ public class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         // Start typing your Java solution below
         // DO NOT write main() function
+        int low = 0, high = matrix.length-1;
+        while (low<=high) {
+            int mid = low + (high - low) / 2;
+            if (matrix[mid][0] == target) return true;
+            if (matrix[mid][0] < target) low = mid+1;
+            else high = mid-1;
+        }
+        if(high<0) return false;
+        int row=high;
+        low=0;high=matrix[0].length-1;
+        while (low<=high) {
+            int mid = low + (high - low) / 2;
+            if (matrix[row][mid] == target) return true;
+            if (matrix[row][mid] < target) low = mid+1;
+            else high = mid-1;
+        }
+        return false;
+    }
+}
+
+
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
         int r=matrix.length-1,c=matrix[0].length-1,lr=0,lc=0,col=c;
         while(lr<=r&&lc<=c) {
             int mr=lr+(r-lr)/2;
