e4726535416d87d0ee43458197545aedd5be64fb
mapan <mapan0817@gmail.com>
2013-09-25 10:31:57 -0400

    Update populatingNextRightPointers.java

--- populatingNextRightPointers.java
+++ populatingNextRightPointers.java
@@ -1,32 +1,3 @@
-/**
- * Definition for binary tree with next pointer.
- * public class TreeLinkNode {
- *     int val;
- *     TreeLinkNode left, right, next;
- *     TreeLinkNode(int x) { val = x; }
- * }
- */
-public class Solution {
-    public void connect(TreeLinkNode root) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        c(root);
-    }
-    public TreeLinkNode c(TreeLinkNode root) {
-        if(root==null||(root.left==null&&root.right==null)) 
-            return root;
-        TreeLinkNode l=c(root.left);
-        TreeLinkNode r=c(root.right);
-        l.next=r;
-        while(l.right!=null) {
-            l.right.next=r.left;
-            l=l.right;
-            r=r.left;
-        }
-        return root;
-    }
-}
-
 public class Solution {
     public void connect(TreeLinkNode root) {
         // Start typing your Java solution below
@@ -56,6 +27,27 @@ public class Solution {
                 }
             }
         }
+    }
+}
+
+public class Solution {
+    public void connect(TreeLinkNode root) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        c(root);
+    }
+    public TreeLinkNode c(TreeLinkNode root) {
+        if(root==null||(root.left==null&&root.right==null)) 
+            return root;
+        TreeLinkNode l=c(root.left);
+        TreeLinkNode r=c(root.right);
+        l.next=r;
+        while(l.right!=null) {
+            l.right.next=r.left;
+            l=l.right;
+            r=r.left;
+        }
+        return root;
     }
 }
 
