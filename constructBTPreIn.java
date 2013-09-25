6d6f27aa1feef8e52f3916de06bb03dd1345ea3b
mapan <mapan0817@gmail.com>
2013-09-24 21:28:39 -0400

    Update constructBTPreIn.java

--- constructBTPreIn.java
+++ constructBTPreIn.java
@@ -21,37 +21,26 @@ public class Solution {
     }
 }
 
-
-public class Solution {   
-    int postindex;
+public class Solution {
     HashMap<Integer,Integer> h;
+    int i;
     public TreeNode buildTree(int[] inorder, int[] postorder) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        postindex=postorder.length-1;
-        
         h=new HashMap<Integer,Integer>();
-        for(int i=0;i<inorder.length;i++) 
-            h.put(inorder[i],i);
-        
-        return build(postorder,inorder,0,postorder.length-1);
+        i=postorder.length-1;
+        for(int j=0;j<inorder.length;j++)
+            h.put(inorder[j],j);
+        return build(0,postorder.length-1,postorder);
     }
-    
-    public TreeNode build(int[] postorder,int[] inorder,int start,int end) {
-        if(start>end) return null;
-        //if(start==end) {preindex++;return new TreeNode(inorder[start]);}
-        TreeNode root=new TreeNode(postorder[postindex--]);
-        if(start==end) return root;
-        int inindex=findInorder(inorder,root.val,start,end);
-        root.right=build(postorder,inorder,inindex+1,end);        
-        root.left=build(postorder,inorder,start,inindex-1);
+    public TreeNode build(int l,int r,int[] post) {
+        if(l>r) return null;
+        int mid=h.get(post[i]);
+        TreeNode root=new TreeNode(post[i]);
+        i--;
+        root.right=build(mid+1,r,post);
+        root.left=build(l,mid-1,post);
         return root;
     }
-    public int findInorder(int[] inorder,int root,int start,int end) {
-        /*for(int i=start;i<=end;i++) 
-            if(inorder[i]==root)
-                return i;
-        return -1;*/
-        return h.get(root);
-    }
 }
+
