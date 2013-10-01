d48032e9912c18f51db7bf69210072a5a0da7358
mapan <mapan0817@gmail.com>
2013-10-01 18:09:50 -0400

    Create palindrome-num.java

---
+++ palindrome-num.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public boolean isPalindrome(int x) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if(x<0) return false;
+        if(x<10) return true;
+        //int divd=10; overflow
+        long divd=10;
+        while(divd<=x) divd*=10;
+        divd/=10;
+        while(divd>1) {
+            //int h=x/divd;
+            //int t=x%10;
+            if(x/divd!=x%10) return false;
+            x=(int)(x%divd)/10;
+            divd/=100;
+        }
+        return true;
+    }
+}
