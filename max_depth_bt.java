a69bc24f830946074b57343863f306608844049c
mapan <mapan0817@gmail.com>
2013-10-04 15:41:50 -0400

    Update max_depth_bt.java

--- max_depth_bt.java
+++ max_depth_bt.java
@@ -2,15 +2,27 @@ public class Solution {
     public int maxDepth(TreeNode root) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        return max(root,0);
-    }
-    public int max(TreeNode root,int depth) {
-        if(root==null) {
-            return depth;
+        if(root==null) return 0;
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.add(root);
+        int nodesNum = 1,level=0;
+        while(!q.isEmpty()) {
+            level++;
+            int j = nodesNum; 
+            nodesNum = 0;
+            for(int i=0;i<j;i++) {
+                TreeNode n = q.remove();
+                if(n.left!=null) {
+                    q.add(n.left);
+                    nodesNum++;
+                }
+                if(n.right!=null) {
+                    q.add(n.right);
+                    nodesNum++;
+                }
+            }
         }
-        int left=max(root.left,depth+1);
-        int right=max(root.right,depth+1);
-        return left>right?left:right;
+        return level;
     }
 }
 
