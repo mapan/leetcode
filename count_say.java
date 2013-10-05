1044b7d733e628fee0c0a921c69f9cb3163c095f
mapan <mapan0817@gmail.com>
2013-10-05 15:45:53 -0400

    Update count_say.java

--- count_say.java
+++ count_say.java
@@ -7,6 +7,29 @@ public class Solution {
         while(n>1) {
             r="";
             for(int i=0;i<pre.length();) {
+                int j=i+1,c=1;
+                while(j<pre.length()&&pre.charAt(j)==pre.charAt(j-1)) {
+                    c++; j++;
+                }
+                i=j;
+                r+=c+""+pre.charAt(j-1);
+            }
+            pre=r;
+            n--;
+        }
+        return r;
+    }
+}
+
+public class Solution {
+    public String countAndSay(int n) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if(n==1) return "1";
+        String pre="1",r="";
+        while(n>1) {
+            r="";
+            for(int i=0;i<pre.length();) {
                 int j=i,c=1;
                 while(j<pre.length()-1&&pre.charAt(j)==pre.charAt(j+1)) {
                     c++; j++;
