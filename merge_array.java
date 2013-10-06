989077b71666b53179df45b8c0e751452842f819
mapan <mapan0817@gmail.com>
2013-10-06 12:19:11 -0400

    Update merge_array.java

--- merge_array.java
+++ merge_array.java
@@ -2,6 +2,20 @@ public class Solution {
     public void merge(int A[], int m, int B[], int n) {
         // Start typing your Java solution below
         // DO NOT write main() function
+        //if(m==0||n==0) return;
+        int i=m+n-1;
+        n--;m--;
+        while(m>=0&&n>=0) {
+            if(A[m]>B[n]) A[i--]=A[m--];
+            else A[i--]=B[n--];
+        }
+        while(n>=0) A[i--]=B[n--];
+    }
+}
+public class Solution {
+    public void merge(int A[], int m, int B[], int n) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
         if(n==0||m==0) {
             if(n!=0) {
                 for(int i=n;i>0;i--)
