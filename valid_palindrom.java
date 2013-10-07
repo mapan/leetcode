bc32a92cae6107194aaedb17dfc26bfc77778877
mapan <mapan0817@gmail.com>
2013-10-06 20:53:25 -0400

    Update valid_palindrom.java

--- valid_palindrom.java
+++ valid_palindrom.java
@@ -1,3 +1,31 @@
+public class Solution {
+    public boolean isPalindrome(String s) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if(s.isEmpty()) return true;
+        int l=0,r=s.length()-1;
+        s=s.toLowerCase();
+        while(l<r) {
+            if(s.charAt(l)==s.charAt(r)) {
+                //if(valid(s.charAt(l)) {
+                    l++; r--;
+                //}
+            }
+            else {
+                if(valid(s.charAt(l))&&valid(s.charAt(r))) return false;
+                if(!valid(s.charAt(l))) l++;
+                if(!valid(s.charAt(r))) r--;
+            }
+        }
+        return true;
+    }
+    public boolean valid(char c) {
+        if((c<'a'||c>'z')&&(c<'0'||c>'9')) return false;
+        if(!((c>='a'&&c<='z')||(c>='0'&&c<='9')))
+        return true;
+    }
+}
+
  /* Remove non-alphanumeric char and change to lowercase  
   * ss = normalize(s) is equivalent to  
   *  ss = s.toLowerCase().replaceAll("[^a-z0-9]", "");  
