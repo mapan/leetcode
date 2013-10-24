6bd9ada1ee21a31d02ed3ce8fc562d05567ac15a
mapan <mapan0817@gmail.com>
2013-10-24 15:29:16 -0400

    Update and rename traversal.c to traversal.java

---
+++ traversal.java
@@ -0,0 +1,58 @@
+void pre(Node root) {
+    while(root!=null) {
+        print(root);
+        if(root.left!=null)
+            root=root.left;
+        else if(root.right!=null)
+            root=root.right;
+        else {
+            Node p=root.parent;
+            while(p!=null&&root==p.right) {
+                root=p;
+                p=p.parent;
+            }
+            if(p==null) break;
+            root=p.right;
+        }
+    }
+}
+
+void in(Node root) {
+    while(root!=null) {
+        while(root.left!=null)
+            root=root.left;
+        print(root);
+        if(root.right!=null)
+            root=root.right;
+        else {
+            Node p=root.parent;
+            while(p!=null&&root==p.right) {
+                root=p;
+                p=p.parent;
+            }
+            if(p==null) break;
+            print(p);
+            root=p.right;
+        }
+    }
+}
+
+void post(Node root) {
+    while(root!=null) {
+        if(root.left!=null) 
+            root=root.left;
+        else if(root.right!=null)
+            root=root.right;
+        else {
+            print(root);
+            Node p=root.parent;
+            while(p!=null&&root==p.right) {
+                print(p);
+                root=p;
+                p=p.parent;
+            }
+            if(p==null) break;
+            root=p.right;
+        }
+    }
+}
