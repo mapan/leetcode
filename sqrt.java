cdc4144e0471045f47435028160534996b508ecd
mapan <mapan0817@gmail.com>
2013-11-11 14:44:36 -0500

    Update sqrt.java

--- sqrt.java
+++ sqrt.java
@@ -2,7 +2,7 @@ public class Solution {
     public int sqrt(int x) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        if (x < 2) return x;
+        //if (x < 2) return x;
         int low = 1, high = x;
         while (low<=high) {
             int mid = low + (high - low) / 2;
